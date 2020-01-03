e107c2d6f0e706ba05c747b95bd772cc27abb888
hunabku-tutounityfr <hunabku.tutounityfr@gmail.com>
2020-01-03 08:06:03 +0100

    Episode 03
    - Mise en place des personnages

---
+++ ThirdPersonShooter_2D/Assets/Scripts/Stats.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    void Start ()
+    {
+        isDead = false;
+        health = maxHealth;
+    }
+    void Update ()
+    {
+        // Damage test
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Damage(10f);
+        }
+    }
+
+    public void Damage (float amount)
+    {
+        if (health <= 0) isDead = true;
+        if (isDead) return;
+        
+        if (health > 0)
+        {
+            health -= amount;
+            healthBar_Bar_Transform.localScale = new Vector3(health / maxHealth, 1f);
+        }
+        
+    }
+
+    [Header("--- Stats ---")]
+    [SerializeField] float health = 0f;
+    [SerializeField] float maxHealth = 100f;
+    [Space]
+    [SerializeField] Transform healthBar_Bar_Transform = null;
+
+    [Header("--- Status ---")]
+    [SerializeField] bool isDead = false;
+}
